56ab9f63ff0bc4b0d0b5bad1345fa9e118c0b008
Niel-77 <nil.panta77@gmail.com>
2024-06-26 18:20:29 +0545

    Used Persisted data

--- ragapp.py
+++ ragapp.py
@@ -1,52 +1,87 @@
 # Import modules
+#What is going under the hood?
 import logging
 import sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
+import nest_asyncio
+nest_asyncio.apply()
 import ollama
 from llama_index.llms.ollama import Ollama
 from pathlib import Path
-import qdrant_client
 from llama_index.core import SimpleDirectoryReader
-from llama_index.core import VectorStoreIndex, ServiceContext, StorageContext, Settings
-from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.core import VectorStoreIndex, StorageContext, Settings, SummaryIndex, load_index_from_storage
 from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.tools import QueryEngineTool
+from llama_index.core.query_engine.router_query_engine import RouterQueryEngine
+from llama_index.core.selectors import LLMSingleSelector
+from llama_index.core.schema import IndexNode
 
-# Create Qdrant client and store
-# client = qdrant_client.QdrantClient(path="./Rag_data")
-# vector_store = QdrantVectorStore(client=client, collection_name="Queries")
-# storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
 import os.path
-from llama_index.core import (
-    VectorStoreIndex,
-    SimpleDirectoryReader,
-    StorageContext,
-    load_index_from_storage,
-)
 
-# Initialize Ollama and ServiceContext
-Settings.llm = Ollama(model="phi3:mini",request_timeout=600,device_map='cuda')
+
+# Initialize Ollama globally
+Settings.llm = Ollama(model="phi3:mini",request_timeout=600,device_map='cuda') #timer set to 600 to prevent timed out error
 Settings.embed_model= OllamaEmbedding(model_name="nomic-embed-text")
 
 # check if storage already exists
 PERSIST_DIR = "./storage"
 if not os.path.exists(PERSIST_DIR):
-    # load the documents and create the index
+    # Load the documents
     documents = SimpleDirectoryReader("Research_Papers").load_data()
-    #Split the documents
+    # Split the documents
+    splitter = SentenceSplitter(chunk_size=1024)
+    nodes = splitter.get_nodes_from_documents(documents)
+    # Create indices
+    summary_index = SummaryIndex(nodes)
+    vector_index = VectorStoreIndex(nodes, embed_model=Settings.embed_model)
     
-    index = VectorStoreIndex.from_documents(documents)
-    # store it for later
-    index.storage_context.persist(persist_dir=PERSIST_DIR)
-else:
-    # load the existing index
-    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-    index = load_index_from_storage(storage_context)
+    # Persist indices
+    summary_index._storage_context.persist(persist_dir=PERSIST_DIR+r"/summary")
+    vector_index._storage_context.persist(persist_dir=PERSIST_DIR+r"/vector")
 
-# Either way we can now query the index
-query_engine = index.as_query_engine()
-response = query_engine.query("Give me the summary of the paper.")
+else:
+    # Load the existing indices
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR+r"/summary")
+    summary_index = load_index_from_storage(storage_context)
+    storage_context1=StorageContext.from_defaults(persist_dir=PERSIST_DIR+r"/vector")
+    vector_index = load_index_from_storage(storage_context1)
+
+#Setting the query engine based on the index stored
+summary_query_engine = summary_index.as_query_engine(
+    response_mode="tree_summarize",
+    use_async=True,
+)
+vector_query_engine = vector_index.as_query_engine()
+
+summary_tool = QueryEngineTool.from_defaults(
+        query_engine=summary_query_engine,
+        description=(
+            "Provide answer to summary related question of the paper"
+        ),
+    )
+    
+vector_tool = QueryEngineTool.from_defaults(
+    query_engine=vector_query_engine,
+    description=(
+        "Provide specific answers related to the paper."
+        ),
+    )
+#route queries to appropriate query engine based on the tools
+query_engine = RouterQueryEngine(
+    selector=LLMSingleSelector.from_defaults(),
+    query_engine_tools=[
+        summary_tool,
+        vector_tool,
+    ],
+    verbose=True
+)
+
+#now query the index
+response = query_engine.query("How good Query2CAD performs with GPT-4 in comparison to GPT-3.5?")
 print(response)
 
 
